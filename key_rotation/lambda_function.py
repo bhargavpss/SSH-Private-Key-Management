1e55eda6285c3d5ac52bab8e13d93a9866d8d161
Ubuntu <ubuntu@ip-172-31-94-128.ec2.internal>
2019-01-17 13:00:58 +0000

    Update

--- key_rotation/lambda_function.py
+++ key_rotation/lambda_function.py
@@ -2,6 +2,7 @@ import boto3
 import sys
 import json
 import base64
+import traceback
 
 from Crypto.PublicKey import RSA
 
@@ -9,24 +10,30 @@ secrets_manager = boto3.client('secretsmanager')
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    response = secrets_manager.list_secrets(
-        MaxResults=99
-    )
+    try:
+    	response = secrets_manager.list_secrets(
+        	MaxResults=99
+    	)
+    except:
+	print traceback.format_exc()
+
     secretslist = {}
     for i in response['SecretList']:
         secretslist[i['Name']] = None
-    
-    response = ec2.describe_instances(
-        Filters=[
-            {
-                'Name': 'instance-state-name',
-                'Values': [
-                    'running','stopped'
-                ]
-            },
-        ]
-    )
-    
+    try:
+    	response = ec2.describe_instances(
+        	Filters=[
+            	    {
+               		'Name': 'instance-state-name',
+                	'Values': [
+                    		'running','stopped'
+                	]
+            	    },
+        	]
+    	)
+    except:
+	print traceback.format_exc()
+
     instancelist = []
     for i in response['Reservations']:
         j = i['Instances']
@@ -41,19 +48,17 @@ def lambda_handler(event, context):
             encoded_key = base64.b64encode(private_key) # base64 encode the private key with no wrap
             pubkey = key.publickey()
             public_key = pubkey.exportKey('OpenSSH')    
-            response = secrets_manager.put_secret_value(
- 	        SecretId=instance_id,
-    	        SecretString='{"public_key":"'+public_key+'","private_key":"'+encoded_key+'"}',
-    	        VersionStages=[
-        	    'AWSCURRENT',
-    	        ]
-	    )
-	  #  print response
-	  #  print '\n'
-
+            try:
+	        response = secrets_manager.put_secret_value(
+ 	        	SecretId=instance_id,
+    	        	SecretString='{"public_key":"'+public_key+'","private_key":"'+encoded_key+'"}',
+    	        	VersionStages=[
+        	    	    'AWSCURRENT',
+    	        	]
+	    	)
+	    except:
+		print traceback.format_exc()
 	else:
 	    continue
 
     return None
-
-# lambda_handler({},None)
