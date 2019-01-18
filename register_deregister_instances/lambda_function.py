971fb1035e57cc4823048ca25c9e00f428e9931e
Ubuntu <ubuntu@ip-172-31-94-128.ec2.internal>
2019-01-18 20:05:24 +0000

    Update

--- register_deregister_instances/lambda_function.py
+++ register_deregister_instances/lambda_function.py
@@ -12,11 +12,11 @@ def lambda_handler(event, context):
 	resources = event['resources']
 	try:
 	    response = secretsmanager.list_secrets()
-        except:
+	except:
 	    print traceback.format_exc()
 	secretslist = {}
-        for i in response['SecretList']:
-            secretslist[i['Name']] = None
+	for i in response['SecretList']:
+        	secretslist[i['Name']] = None
 
 	if event['detail']['state'] == 'running':
 	    key = RSA.generate(2048)                   # Generating Key Pair
@@ -27,32 +27,36 @@ def lambda_handler(event, context):
 	    for arn in resources:
 	        instance_id = arn.split('/')[1]
 	        if instance_id in secretslist:            # Don't do anything if the key is already present. Could be a stopped instance which is just started
-	            continue
+	        	continue
 	        else:
-		    try:
-		    	response = ec2.describe_instances(
-                		InstanceIds=[
-        				instance_id,
-    				],
-		    	)
-		    except:
-			print traceback.format_exec()
+			try:
+				response = ec2.describe_instances(
+                			InstanceIds=[
+        					instance_id,
+    					],
+		    		)
+			except:
+				print traceback.format_exec()
 
+		    found = False
 		    for i in  response['Reservations'][0]['Instances'][0]['Tags']:
 		        if i['Key'] == 'microservice':
 		       	    microservice = i['Value']
+			    found = True
         		    break
     			else:
         		    continue
-		    
+		    if not found:
+			microservice = 'general'
+
 		    try:		    
-    	                response = secretsmanager.create_secret(
+    	        	response = secretsmanager.create_secret(
 	                    Name=instance_id,
 	                    Description='SSH Key Pair for Instance',
                             SecretString='{"public_key":"'+public_key+'","private_key":"'+encoded_key+'"}',
 	                    Tags=[
 	                            {
-	           	                'Key': 'InstanceARN',
+	     		                'Key': 'InstanceARN',
 			                'Value': arn
 			            },
 				    {
@@ -62,7 +66,7 @@ def lambda_handler(event, context):
 			         ]
 		        )	
 		    except:
-			print traceback.format_exec()
+		        print traceback.format_exec()
 
             return None
 
@@ -71,13 +75,13 @@ def lambda_handler(event, context):
 	        instance_id = arn.split('/')[1]
 	        if instance_id in secretslist:
 	            try:
-			response = secretsmanager.delete_secret(
+		        response = secretsmanager.delete_secret(
 	                    SecretId=instance_id,
 	                    RecoveryWindowInDays=7,
 	                    ForceDeleteWithoutRecovery=False
 	                )
 		    except:
-			print traceback.format_exec()
+		        print traceback.format_exec()
 
 	        else:                                # Don't do anything if the key is not present. Instance is not registered in SecretsManager
 	    	    continue
