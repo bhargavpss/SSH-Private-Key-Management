cc89bb99253c94b038d8259932a8531fc6c33996
Ubuntu <ubuntu@ip-172-31-94-128.ec2.internal>
2019-01-21 20:57:06 +0000

    Updating lambda_function

--- register_deregister_instances/lambda_function.py
+++ register_deregister_instances/lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 import base64
 import traceback
 from os import chmod
+from botocore.exceptions import ClientError
 from Crypto.PublicKey import RSA
 
 secretsmanager = boto3.client('secretsmanager')
@@ -10,83 +11,80 @@ ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
 	resources = event['resources']
-	try:
-		response = secretsmanager.list_secrets()
-	except:
-		print traceback.format_exc()
-	secretslist = {}
-	for i in response['SecretList']:
-		secretslist[i['Name']] = None
 
 	if event['detail']['state'] == 'running':
-		key = RSA.generate(2048)                   # Generating Key Pair
+		# Generating Key Pair
+		key = RSA.generate(2048)
 		private_key = key.exportKey('PEM')
-		encoded_key = base64.b64encode(private_key) # base64 encode the private key with no wrap
+		# base64 encode the private key with no wrap
+		encoded_key = base64.b64encode(private_key)
 		pubkey = key.publickey()
 		public_key = pubkey.exportKey('OpenSSH')
+		
 		for arn in resources:
 			instance_id = arn.split('/')[1]
-			if instance_id in secretslist:            # Don't do anything if the key is already present. Could be a stopped instance which is just started
-				continue
-			else:
-				try:
-					response = ec2.describe_instances(
-						InstanceIds=[
-							instance_id,
+			try:
+				response = ec2.describe_instances(
+					InstanceIds=[
+						instance_id,
     					],
-					)
-				except:
-					print traceback.format_exec()
+				)
+			except ClientError as e:
+				raise e
 
-			found = False
+			microservice_tag_exists = False
 			for i in  response['Reservations'][0]['Instances'][0]['Tags']:
 				if i['Key'] == 'microservice':
 					microservice = i['Value']
-					found = True
+					microservice_tag_exists = True
 					break
 				else:
 					continue
-			if not found:
+			if not microservice_tag_exists:
+				# Tagging all untagged instances with 'general'
 				microservice = 'general'
 
 			try:		    
 				response = secretsmanager.create_secret(
-						Name=instance_id,
-						Description='SSH Key Pair for Instance',
-						SecretString='{"public_key":"'+public_key+'","private_key":"'+encoded_key+'"}',
-						Tags=[
-							{
-								'Key': 'InstanceARN',
-								'Value': arn
-							},
-							{
-								'Key': 'microservice',
-								'Value': microservice
-							}
-						]
+					Name=instance_id,
+					Description='SSH Key Pair for Instance',
+					SecretString='{"public_key":"'+public_key+'","private_key":"'+encoded_key+'"}',
+					Tags=[
+						{
+							'Key': 'InstanceARN',
+							'Value': arn
+						},
+						{
+							'Key': 'microservice',
+							'Value': microservice
+						}
+					]
 				)
-			except:
-				print traceback.format_exec()
+			except ClientError as e:
+                                if e.response['Error']['Code'] == "ResourceExistsException":
+                                        # Don't do anything if the secret is already present.
+                                        # Could be a stopped instance which is just started
+                                        continue
+				else:
+					raise e
 
 		return None
 
 	elif event['detail']['state'] == 'terminated':
 		for arn in resources:
 			instance_id = arn.split('/')[1]
-			if instance_id in secretslist:
-				try:
-					response = secretsmanager.delete_secret(
-						SecretId=instance_id,
-						RecoveryWindowInDays=7,
-						ForceDeleteWithoutRecovery=False
-					)
-				except:
-					print traceback.format_exec()
-					
-			else:                                # Don't do anything if the key is not present. Instance is not registered in SecretsManager
-				continue
+			try:
+				response = secretsmanager.delete_secret(
+					SecretId=instance_id,
+					RecoveryWindowInDays=7,
+					ForceDeleteWithoutRecovery=False
+				)
+			except ClientError as e:
+				if e.response['Error']['Code'] == 'ResourceNotFoundException':
+					# Don't do anything if the secret is not present
+					# Instance may not have registered in SeretsManager
+					continue
+				else:
+					raise e
 		
 		return None
-
-
-
